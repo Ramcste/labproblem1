3c708a9652a364411f97f0aafe689651fd607c97
ramcste <lucky.guyram@gmail.com>
2015-04-22 12:29:52 +0600

    first commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Upper and Lower limit of the Numbers:");
+            int upperlimit = Convert.ToInt32(Console.ReadLine());
+            int lowerlimit = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = lowerlimit+1; i <upperlimit ; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
